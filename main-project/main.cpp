262113b1abf7f550648a9a621a5cb57b0d50ada3
ivanovkiri11 <165017527+ivanovkiri11@users.noreply.github.com>
2024-05-30 11:28:18 +0300

    Добавлен вывод общей информации о задании

---
+++ main-project/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Laboratory work #10. GIT\n";
+    cout << "Variant #7. Library Subscription\n";
+    cout << "Author: Ivanov Kirill\n";
+    return 0;
+}
