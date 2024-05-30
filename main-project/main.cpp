81e1711bb543621c69ddd3e26c085c8fe86f5b2c
ivanovkiri11 <165017527+ivanovkiri11@users.noreply.github.com>
2024-05-30 13:48:28 +0300

    Update main.cpp

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #10. GIT\n";
     cout << "Variant #7. Bank Operations\n";
     cout << "Author: Ivanov Kirill\n";
+    cout << "Group: 23UIR1d\n";
     return 0;
 }
