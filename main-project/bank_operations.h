cbf58aaaee8b92f81aade2279f61b65bf579b314
ivanovkiri11 <165017527+ivanovkiri11@users.noreply.github.com>
2024-05-30 13:46:01 +0300

    Update bank_operations.h

--- main-project/bank_operations.h
+++ main-project/bank_operations.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include <string>
+
+struct date {
+    int day;
+    int month;
+    int year;
+};
+
+struct bank_operations {
+    int hour;
+    int minute;
+    int seconds;
+    date start;
+    date finish;
+    std::string title;
+    std::string action;
+    int number;
+    double money;
+    std::string typeofaction; 
+};
